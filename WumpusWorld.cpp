0335ad2729ff283db6bad000f0a15e5b904d9899
BrunoHSL007 <brunohenrique.laier28@gmail.com>
2017-10-20 22:13:52 -0200

    v0.2 Colocadas classes para o Mapa, Player e Wumpus

--- WumpusWorld.cpp
+++ WumpusWorld.cpp
@@ -1,32 +1,56 @@
 //Wumpus World - Artificial Intelligence
 //Bruno Henrique dos Santos Laier
-//Version 0.1 18/10/2017
+//Version 0.2 20/10/2017
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
 
-int Linha=10;
-int Coluna=10;
+bool IAGameMode=true;//IAGameMode true = player is IA false = normal player
+
+const int Linha=10;
+const int Coluna=10;
 
 const int player=1;
 const int ouro=2;
-const int brisa=3;
+const int brilho=3;
 const int wumpus=4;
 const int fedor=5;
 const int buraco=6;
+const int brisa=7;
 
-int **inicializaMatriz(int** Mapa){
-	for(int i=0;i< Linha ;i++)
+const int PosVisitada=-1;
+
+int NroBuracos=4;
+
+
+class Wumpus{
+	private:
+		
+		//int WumpusMap[Linha][Coluna]; //posições por onde o wumpus já passou e posição atual
+		int PosiLinhaW; // Linha em que o wumpus está
+		int PosiColW;   // Coluna em que o wumpus está
+			
+	public:
+		Wumpus();
+		int getPosiLinhaW(); // Linha em que o wumpus está
+		int getPosiColW();   // Coluna em que o wumpus está
+		//void moveW();
+		//void attackW();
+		
+		
+};
+Wumpus::Wumpus(){
+	/*
+	for(int i=0; i<Linha;i++)
 	{
 		for(int j=0;j<Coluna;j++)
 		{
-			Mapa[i][j]=0;
+			WumpusMap[i][j]=0;
 		}
 	}
+	*/
 	int Linerand,Colrand;
-	
-	//Colocando o Wumpus no mapa
 	srand( (unsigned)time(NULL) );
 	int aux=rand();
 	Linerand =(aux % static_cast<int>(Linha));
@@ -36,76 +60,274 @@ int **inicializaMatriz(int** Mapa){
 	};
 	aux=rand();
 	Colrand = (aux % static_cast<int>(Coluna));
-	Mapa[Linerand][Colrand]=wumpus;
-	if(Linerand-1>=0)
-		Mapa[Linerand-1][Colrand]=fedor;
-	if(Linerand+1<Linha)	
-		Mapa[Linerand+1][Colrand]=fedor;
-	if(Colrand-1>=0)
-		Mapa[Linerand][Colrand-1]=fedor;
-	if(Colrand+1<Coluna)
-		Mapa[Linerand][Colrand+1]=fedor;
+	//WumpusMap[Linerand][Colrand]=wumpus;
+	PosiLinhaW=Linerand;
+	PosiColW=Colrand;
 	
-	int LinerandOuro,ColrandOuro;
-	//while(rand()!=aux)
-	//{
-	//	srand( (unsigned)time(NULL) );
-	//};
-	do
-	{
-		aux=rand();
-		//Colocando o pote de ouro no mapa
-		LinerandOuro =(aux % static_cast<int>(Linha));
-		//while(rand()!=aux)
-		//{
-		//	srand( (unsigned)time(NULL) );
-		//};
-		aux=rand();
-		ColrandOuro = (aux % static_cast<int>(Coluna));
-	}
-	while((ColrandOuro==Colrand) && (Linerand==LinerandOuro));
-	Mapa[LinerandOuro][ColrandOuro]=ouro;
-	if(LinerandOuro-1>=0)
-		Mapa[LinerandOuro-1][ColrandOuro]=brisa;
-	if(LinerandOuro+1<Linha)	
-		Mapa[LinerandOuro+1][ColrandOuro]=brisa;
-	if(ColrandOuro-1>=0)
-		Mapa[LinerandOuro][ColrandOuro-1]=brisa;
-	if(ColrandOuro+1<Coluna)
-		Mapa[LinerandOuro][ColrandOuro+1]=brisa;
 	
-	return Mapa;
 }
-int **alocaMatriz(int l, int c)
+
+int Wumpus::getPosiLinhaW(){
+	return PosiLinhaW;
+}
+int Wumpus::getPosiColW(){
+	return PosiColW;
+}
+/*Wumpus::moveW(){
+	if(PosiLinhaW!=0)// Para cima
+	{
+		if(WumpusMap[PosiLinhaW-1][PosiColW]>=0)
+		{
+			WumpusMap[PosiLinhaW-1][PosiColW]=wumpus;
+			WumpusMap[PosiLinhaW][PosiColW]=PosVisitada;
+			
+		}
+	}
+	else if(PosiColW!=0)// Para esquerda
+	{
+		if(WumpusMap[PosiLinhaW][PosiColW-1]>=0)
+		{
+			WumpusMap[PosiLinhaW][PosiColW-1]=wumpus;
+			WumpusMap[PosiLinhaW][PosiColW]=PosVisitada;
+			
+		}
+	}
+	else if(PosiLinhaW!=Linha-1)// Para baixo
+	{
+		if(WumpusMap[PosiLinhaW+1][PosiColW]>=0)
+		{
+			WumpusMap[PosiLinhaW+1][PosiColW]=wumpus;
+			WumpusMap[PosiLinhaW][PosiColW]=PosVisitada;
+			
+		}
+	}
+	else if(PosiColW!=Coluna-1)// Para direita
+	{
+		if(WumpusMap[PosiLinhaW][PosiColW+1]>=0)
+		{
+			WumpusMap[PosiLinhaW][PosiColW+1]=wumpus;
+			WumpusMap[PosiLinhaW][PosiColW]=PosVisitada;
+			
+		}
+	}
+	
+}
+
+Wumpus::attackW(){
+	cout << "Você foi morto pelo Wumpus";
+}*/
+
+
+class WorldMAP : public Wumpus
 {
-	int i;
+	private:
+		int Mapa[Linha][Coluna];
+	public:
+		WorldMAP();
+		void printMapa(); //Ver questão da herança
+};
+WorldMAP::WorldMAP()
+{
+	Wumpus();
+	/*int i;
 	int **A;
 	 
-	A = (int **) calloc(l, sizeof(int *)); 
+	A = (int **) calloc(Linha, sizeof(int *)); 
 	for(i = 0; i < l; i++) 
-		A[i] = (int *) calloc(c, sizeof(int)); 
+		A[i] = (int *) calloc(Coluna, sizeof(int)); 
 	
-	return A; 
-
-}
-void PrintMatriz(int **Mapa)
-{
-	for(int i=0;i<Linha;i++)
+	return A;*/
+	
+	//Inicializando a matriz com 0
+	for(int i=0;i< Linha ;i++)
 	{
 		for(int j=0;j<Coluna;j++)
 		{
-			cout << Mapa[i][j] << " ";
+			Mapa[i][j]=0;
+		}
+	}
+	//Colocando o Wumpus no mapa
+	int auxLinha = getPosiLinhaW();
+	int auxColuna = getPosiColW();
+	Mapa[auxLinha][auxColuna]=wumpus;
+	if(auxLinha-1>=0)
+		Mapa[auxLinha-1][auxColuna]+=fedor;
+	if(auxLinha+1<Linha)	
+		Mapa[auxLinha+1][auxColuna]+=fedor;
+	if(auxColuna-1>=0)
+		Mapa[auxLinha][auxColuna-1]+=fedor;
+	if(auxColuna+1<Coluna)
+		Mapa[auxLinha][auxColuna+1]+=fedor;
+	
+	//Colocando o pote de Ouro no mapa
+	srand( (unsigned)time(NULL) );
+	int aux=rand();
+	int LinerandOuro,ColrandOuro;
+	aux=rand();
+	LinerandOuro =(aux % static_cast<int>(Linha));
+	aux=rand();
+	ColrandOuro = (aux % static_cast<int>(Coluna));
+	Mapa[LinerandOuro][ColrandOuro]+=ouro;
+	if(LinerandOuro-1>=0)
+		Mapa[LinerandOuro-1][ColrandOuro]+=brilho;
+	if(LinerandOuro+1<Linha)	
+		Mapa[LinerandOuro+1][ColrandOuro]+=brilho;
+	if(ColrandOuro-1>=0)
+		Mapa[LinerandOuro][ColrandOuro-1]+=brilho;
+	if(ColrandOuro+1<Coluna)
+		Mapa[LinerandOuro][ColrandOuro+1]+=brilho;
+	int LinerandBuraco, ColrandBuraco;
+	
+	//Colocando os buracos no mapa
+	for(int i=0;i<NroBuracos;i++)
+	{
+		aux=rand();
+		LinerandBuraco =(aux % static_cast<int>(Linha));
+		aux=rand();
+		ColrandBuraco = (aux % static_cast<int>(Coluna));
+		Mapa[LinerandBuraco][ColrandBuraco]+=buraco;
+		if(LinerandBuraco-1>=0 && Mapa[LinerandBuraco-1][ColrandBuraco]==0)
+			Mapa[LinerandBuraco-1][ColrandBuraco]+=brisa;
+		if(LinerandBuraco+1<Linha && Mapa[LinerandBuraco+1][ColrandBuraco]==0)	
+			Mapa[LinerandBuraco+1][ColrandBuraco]+=brisa;
+		if(ColrandBuraco-1>=0 && Mapa[LinerandBuraco][ColrandBuraco-1]==0)
+			Mapa[LinerandBuraco][ColrandBuraco-1]+=brisa;
+		if(ColrandBuraco+1<Coluna && Mapa[LinerandBuraco][ColrandBuraco+1]==0)
+			Mapa[LinerandBuraco][ColrandBuraco+1]+=brisa;
+	}
+
+}
+
+void WorldMAP::printMapa()
+{
+	bool flag=false;
+	cout << "┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐" << endl;
+	for(int i=0;i<Linha;i++)
+	{
+		
+		if(flag==false)
+			flag=true;
+		else
+			cout << "├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤" << endl;
+			
+		cout <<"│ ";
+		for(int j=0;j<Coluna;j++)
+		{
+			cout << Mapa[i][j] << " │ ";
 		}
 		cout << endl;
 	}
+	cout << "└───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘" << endl;
+}
+
+
+class Player : public WorldMAP{
+	private:
+		int PlayerMap[Linha][Coluna];
+		int PosiLinha;
+		int PosiCol;		
+	public:
+		Player();
+		void moveIA();
+		void UP();
+		void DOWN();
+		void RIGHT();
+		void LEFT();
+		
+		
+		
+};
+
+Player::Player(){
+	for(int i=0; i<Linha;i++)
+	{
+		for(int j=0;j<Coluna;j++)
+		{
+			PlayerMap[i][j]=0;
+		}
+	}
+	PlayerMap[0][0]=player;	
+}
+
+void Player::UP(){
+	if(PosiLinha!=0)
+	{
+		PlayerMap[PosiLinha-1][PosiCol]=player;
+		PlayerMap[PosiLinha][PosiCol]=PosVisitada;
+		
+	}
+}
+void Player::DOWN(){
+	if(PosiLinha!=Linha-1)
+	{
+		PlayerMap[PosiLinha+1][PosiCol]=player;
+		PlayerMap[PosiLinha][PosiCol]=PosVisitada;
+		
+	}
+}
+void Player::RIGHT(){
+	if(PosiCol!=Coluna-1)
+		{
+			PlayerMap[PosiLinha][PosiCol+1]=player;
+			PlayerMap[PosiLinha][PosiCol]=PosVisitada;
+			
+		}
+}
+void Player::LEFT(){
+	if(PosiCol!=0)
+	{
+		PlayerMap[PosiLinha][PosiCol-1]=player;
+		PlayerMap[PosiLinha][PosiCol]=PosVisitada;
+		
+	}
+}
+
+void Player::moveIA(){
+	
+	/*if(PosiLinhaW!=0)// Para cima
+	{
+		if(WumpusMap[PosiLinhaW-1][PosiColW]>=0)
+		{
+			WumpusMap[PosiLinhaW-1][PosiColW]=wumpus;
+			WumpusMap[PosiLinhaW][PosiColW]=PosVisitada;
+			
+		}
+	}
+	else if(PosiColW!=0)// Para esquerda
+	{
+		if(WumpusMap[PosiLinhaW][PosiColW-1]>=0)
+		{
+			WumpusMap[PosiLinhaW][PosiColW-1]=wumpus;
+			WumpusMap[PosiLinhaW][PosiColW]=PosVisitada;
+			
+		}
+	}
+	else if(PosiLinhaW!=Linha-1)// Para baixo
+	{
+		if(WumpusMap[PosiLinhaW+1][PosiColW]>=0)
+		{
+			WumpusMap[PosiLinhaW+1][PosiColW]=wumpus;
+			WumpusMap[PosiLinhaW][PosiColW]=PosVisitada;
+			
+		}
+	}
+	else if(PosiColW!=Coluna-1)// Para direita
+	{
+		if(WumpusMap[PosiLinhaW][PosiColW+1]>=0)
+		{
+			WumpusMap[PosiLinhaW][PosiColW+1]=wumpus;
+			WumpusMap[PosiLinhaW][PosiColW]=PosVisitada;
+			
+		}
+	}*/
 }
 int main()
 {
-	int **Mapa;
-	Mapa=alocaMatriz(Linha,Coluna);
-	
-	
-	inicializaMatriz(Mapa);
-	PrintMatriz(Mapa);
+	WorldMAP GameMap;
+	GameMap.printMapa();
+	//int **Mapa;
+	//Mapa=alocaMatriz(Linha,Coluna);
+	//inicializaMatriz(Mapa);
+	//PrintMatriz(Mapa);
 	return 0;
 }
